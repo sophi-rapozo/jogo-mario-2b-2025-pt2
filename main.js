85f1c8d264a58a94c817843d4f4fa8bb3b6ee562
sophi-rapozo <sophia.rapozo@escola.pr.gov.br>
2025-07-11 15:04:30 -0300

    .

--- main.js
+++ main.js
@@ -15,6 +15,59 @@ const startGame = () => {
     gameStarted = true;
     audioStart.play();
 
-    pipe.computedStyleMap.animation ="pipe-animation 1.5s"
+    pipe.computedStyleMap.animation ="pipe-animation 1.5s infinite linear";
 
+    starButton.style.display = "none";
+    mario.style.opacity = "1";
+    pipe.style.opacity = "1";
+    nuvem.style.opacity ="1";
+}
+
+
+const jump = () => {
+    mario.classList.add("jump");
+
+    setTimeout(() => {
+        mario.classList.remove("jump");
+    }
+    ,500);
+}
+
+const updateScore = () => {
+    score += 1;
+    scoreElement.textContent = score;
+    const animationSpeed = 1.5/(1 + score/500);
+    pipe.style.animation = pipe-animation ${animationSpeed}s infinite linear;
+}
+
+const loop = setInterval (() => {
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace("px","");
+}
+if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80){
+    pipe.style.animation = "none";
+    pipe.style.left = '${pipePosition}px';
+
+    mario.style.animation = "none";
+    mario.style.left = '${marioPosition}px';
+
+    mario.src = "./imagem/game-over.png";
+    mario.style.width = "75px";
+    mario.style.marginLeft = "50px";
+
+    audioStart.pause();
+    gameOverSound.play();
+
+    clearInterval(loop);
+    gameOverScreen.style.display = "flex";
+} else if (pipePosition < 0 && gameStarted) {
+    updateScore();
+    pipe.style.left = '';
+}
+},10);
+
+document.addEventListener("keydown, jump");
+
+const restarGame = () => {
+    window.location.reload();
 }
